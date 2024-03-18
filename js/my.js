e7db4f886267bfd3fa49ec87f4a156af3ca7b0c1
yourahmeddev <ahmedhassansheikh7@gmail.com>
2024-03-19 00:44:51 +0500

    learning jQuery Today

---
+++ js/my.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+  // $("p").click(function () {
+  //   console.log("you clicked on p",this);
+  //   // $("#id").hide()
+  //   // $(".class").hide()
+  // });
+  // id selector in jquery
+  // $("#second").click(function(){
+  //     console.log(this)
+  // });
+  // class selector in jquery
+  // $(".fourth").click(function(){
+  //     console.log(this)
+  // });
+  // there are three main types of selector in jquery
+  // element selector
+  // id selector
+  // class selector
+  // Events in jquery
+  // Mouse events
+  // click
+  // double click
+  // $("button").dblclick(function(){
+  //     $("p").hide();
+  // })
+  $("button").mouseenter(function () {
+    $(p).css("color", "red");
+  });
+  // mouse enter
+  // mouse leave
+  // keyboard event
+  // keypress
+  // keydown
+  // key up
+  // form events
+  // submit
+  // focus
+  // blur
+  // change
+  // docuemnet / window events
+  // load , resize, scroll, unload
+});
